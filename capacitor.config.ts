fe84fcee89e4325a19d17b35efe57f365fc60503
santosjp10 <136762618+santosjp10@users.noreply.github.com>
2026-07-18 22:34:43 -0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'br.edu.uft.geocampo',
-  appName: 'GeoCampo',
-  webDir: 'www'
-};
-
-export default config;
